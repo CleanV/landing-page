7c0d54243b739047d1e7a23dceacee5acaaade91
Davkur <davkur2256@gmail.com>
2023-12-12 20:14:08 +0800

    add event

---
+++ src/data.js
@@ -0,0 +1,51 @@
+import Kali1 from './assets/img/acara/kali1.jpg';
+import Kali2 from './assets/img/acara/kali2.jpg';
+import Kali3 from './assets/img/acara/kali3.jpg';
+import Pantai1 from './assets/img/acara/pantai1.jpg';
+import Pantai2 from './assets/img/acara/pantai2.jpeg';
+import Pantai3 from './assets/img/acara/pantai3.jpg';
+
+export default [
+  {
+    date: 14,
+    month: 'apr',
+    title: 'Pembersihan sampah di Kali Ciliwung',
+    desc: 'Ayo pesan tiket sekarang untuk ikut acara kebersihan ini!',
+    img: Kali1
+  },
+  {
+    date: 20,
+    month: 'aug',
+    title: 'Pembersihan Pantai Timur Ancol',
+    desc: 'Ayo pesan tiket sekarang untuk ikut acara kebersihan ini!',
+    img: Pantai1
+  },
+  {
+    date: 18,
+    month: 'sep',
+    title: 'Kerja Bakti Sungai Citarum',
+    desc: 'Ayo pesan tiket sekarang untuk ikut acara kebersihan ini!',
+    img: Kali2
+  },
+  {
+    date: 14,
+    month: 'apr',
+    title: 'Aksi Mahasiswa UNUD Bersih-Bersih Pantai Padanggalak',
+    desc: 'Ayo pesan tiket sekarang untuk ikut acara kebersihan ini!',
+    img: Pantai2
+  },
+  {
+    date: 20,
+    month: 'aug',
+    title: 'Pembersihan Pantai Losari',
+    desc: 'Ayo pesan tiket sekarang untuk ikut acara kebersihan ini!',
+    img: Pantai3
+  },
+  {
+    date: 18,
+    month: 'sep',
+    title: 'Pembersihan sampah di Selokan Jakarta Utara',
+    desc: 'Ayo pesan tiket sekarang untuk ikut acara kebersihan ini!',
+    img: Kali3
+  },
+];
--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,11 +1,13 @@
 import Icon from '../assets/img/Icon.png';
+import Gambar1 from '../assets/img/acara/kali1.jpg';
+import MockEventData from '../data';
 
 const Index = () => {
   return (
     <div className="w-screen max-w-full">
       {/* Hero Banner */}
       <div className="hero-bg">
-        <div className="grid grid-cols-2 h-screen pt-10">
+        <div className="grid grid-cols-2 h-[90vh] pt-10">
 
           <div className="flex">
             <img src={Icon} alt="hero" className="object-contain w-1/2 my-auto ml-auto mr-24" />
@@ -25,7 +27,7 @@ const Index = () => {
       {/* Hero Banner */}
 
       {/* Search Bar */}
-      <div className="bg-[#242565] text-white mx-60 py-7 px-11 rounded-xl relative -top-14">
+      <div className="bg-[#242565] text-white mx-60 py-7 px-11 rounded-xl relative -top-14 shadow-2xl">
         <div className="grid grid-cols-3 gap-10">
           <div>
             <p>Search Event</p>
@@ -44,6 +46,48 @@ const Index = () => {
         </div>
       </div>
       {/* Search Bar */}
+
+      {/* Upcoming Events */}
+      <div className="mx-60 my-16">
+        <div className="flex items-center justify-between mb-24">
+          <p className="float-left text-4xl text-[#242565] w-max">Upcoming Events</p>
+          <div className="float-right">
+            <select id="weekdays" name="weekdays" className="combo-events">
+              <option value="" selected>Weekdays</option>
+            </select>
+            <select id="event-type" name="event_type" className="combo-events">
+              <option value="" selected>Event Type</option>
+            </select>
+            <select id="category" name="category" className="combo-events">
+              <option value="" selected>Any Category</option>
+            </select>
+          </div>
+        </div>
+
+        <div className="grid grid-cols-3 gap-8">
+          {MockEventData.map((v, i) => <div key={i}>
+            <div className="grid grid-cols-1">
+              <img src={v.img} alt="gambar" className="aspect-video object-cover rounded-t-2xl" />
+              <div className="grid grid-cols-6 py-5 px-6">
+                <div className="text-center">
+                  <p className="text-sm uppercase">{v.month}</p>
+                  <p className="text-3xl">{v.date}</p>
+                </div>
+                <div className="col-span-5 pl-5">
+                  <p className="font-bold text-lg">{v.title}</p>
+                  <p className="my-2">{v.desc}</p>
+                </div>
+              </div>
+            </div>
+          </div>)}
+        </div>
+
+
+        <div className="w-full flex">
+          <button className="btn-normal mt-10 mb-16 mx-auto">Load More</button>
+        </div>
+      </div>
+      {/* Upcoming Events */}
     </div>
   )
 };
