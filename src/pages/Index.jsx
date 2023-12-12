9ae22a565f581778abdfef56c1ee1dacaac50a4b
Davkur <davkur2256@gmail.com>
2023-12-12 21:51:01 +0800

    add content

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,5 +1,6 @@
 import Icon from '../assets/img/Icon.png';
-import Gambar1 from '../assets/img/acara/kali1.jpg';
+import Konsul from '../assets/img/konsul.png';
+import Brand from '../assets/img/brand.png';
 import MockEventData from '../data';
 
 const Index = () => {
@@ -84,10 +85,57 @@ const Index = () => {
 
 
         <div className="w-full flex">
-          <button className="btn-normal mt-10 mb-16 mx-auto">Load More</button>
+          <button className="btn-normal mt-10 mx-auto">Load More</button>
         </div>
       </div>
       {/* Upcoming Events */}
+
+      {/* Make your own Event */}
+      <div className="mt-16 grid grid-cols-2 gap-7 bg-[#25CC22] bg-opacity-50">
+        <div className='flex'>
+          <img src={Konsul} alt="konsul" className='w-1/2 ml-auto' />
+        </div>
+        <div className='py-6 flex'>
+          <div className="my-auto">
+            <p className='text-3xl font-bold'>Make your own Event</p>
+            <p className='text-lg mt-2 mb-4'>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            <button className="btn-hero-primary">Create Events</button>
+          </div>
+        </div>
+      </div>
+      {/* Make your own Event */}
+
+      {/* Brands */}
+      <div className="mx-60 my-20 text-center">
+        <p className='text-4xl text-[#242565] mb-7'>Join these brands</p>
+        <p>We&apos;ve had the pleasure of working with industry-defining brands. These<br/>are just some of them</p>
+        <img src={Brand} alt="brand" className='mt-20' />
+      </div>
+      {/* Brands */}
+
+      {/* Blog */}
+      <div className="mx-60 my-28 text-center">
+        <p className='text-4xl text-[#242565] mb-7'>Blog</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+
+        <div className="grid grid-cols-3 gap-10 mt-14">
+          {MockEventData.slice(0,3).map((v, i) => <div key={i}>
+            <div className="grid grid-cols-1 gap-6">
+              <img src={v.img} alt="gambar" className="aspect-video object-cover rounded-2xl" />
+              <div className='text-left'>
+                <p className='text-[#242565] text-xl font-bold'>{v.title}</p>
+                <p className='my-4'>{v.desc}</p>
+                <p className='text-sm text-[#A5A5A5]'>{v.date} <span className='uppercase'>{v.month}</span> - John Doe</p>
+              </div>
+            </div>
+          </div>)}
+        </div>
+
+        <div className="w-full flex">
+          <button className="btn-normal mt-10 mx-auto">Load More</button>
+        </div>
+      </div>
+      {/* Blog */}
     </div>
   )
 };
