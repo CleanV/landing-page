d14366ed9d6f40ff56f91f097d83b8aaf9ef6e67
Davkur <davkur2256@gmail.com>
2023-12-14 21:11:35 +0800

    add responsive to footer

--- src/components/FooterComponent.jsx
+++ src/components/FooterComponent.jsx
@@ -1,19 +1,19 @@
 const FooterComponent = () => {
   return (
     <div className="w-screen bg-[#25CC22] max-w-full text-white">
-      <div className="grid grid-cols-6 gap-9">
-        <div className="col-span-2 py-12 px-32">
+      <div className="grid lg:grid-cols-6 grid-cols-1 gap-9">
+        <div className="col-span-2 lg:py-12 py-4 lg:px-32 px-10 lg:text-left text-center">
           <p className="text-3xl font-bold mb-5">CleanV</p>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
         </div>
-        <div className="py-12 px-14">
+        <div className="lg:py-12 py-2 lg:px-14 px-10 lg:col-span-1 col-span-2 lg:text-left text-center">
           <p className="font-bold mb-6">Plan Events</p>
           <p>Create and Set Up</p>
           <p>Sell Tickets</p>
           <p>Online RSVP</p>
           <p>Online Events</p>
         </div>
-        <div className="py-12 px-14">
+        <div className="lg:py-12 py-2 lg:px-14 px-10 lg:col-span-1 col-span-2 lg:text-left text-center">
           <p className="font-bold mb-6">CleanV</p>
           <p>About Us</p>
           <p>Press</p>
@@ -23,12 +23,13 @@ const FooterComponent = () => {
           <p>Privacy</p>
           <p>Terms</p>
         </div>
-        <div className="col-span-2 py-12 px-32">
+        <div className="col-span-2 lg:py-12 py-4 lg:px-32 px-10 lg:text-left text-center">
           <p className="text-3xl font-bold mb-5">Stay In The Loop</p>
           <p>Join our mailing list to stay in the loop with our newest for Event.</p>
           <button className="btn-hero-secondary mt-5">Subscribe Now</button>
         </div>
       </div>
+      <p className="text-center py-4 text-sm">(C) 2023 CleanV - All rights reserved.</p>
     </div>
   )
 }
